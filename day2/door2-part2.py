3056768f9360ffcc5913e65d59e1506a5934ce82
Defqon51 <77271530+Defqon51@users.noreply.github.com>
2021-12-10 23:08:53 +0100

    Update door2-part2.py

--- day2/door2-part2.py
+++ day2/door2-part2.py
@@ -12,20 +12,6 @@ horizontalPosition = 0
 depth = 0
 aim = 0
 
-"""
-testList = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
-
-for i in range(0, len(testList)):
-    currentCommand = str(testList[i])
-    if currentCommand.startswith("forward"):
-        horizontalPosition += int(currentCommand[-1])
-        depth += int(currentCommand[-1]) * aim
-    elif currentCommand.startswith("down"):
-        aim += int(currentCommand[-1])
-    elif currentCommand.startswith("up"):
-        aim -= int(currentCommand[-1])
-"""
-
 for i in range(0, len(commandList)):
     currentCommand = str(commandList[i])
     if currentCommand.startswith("forward"):
